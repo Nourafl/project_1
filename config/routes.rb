2fe65721de8ac70c210f529ce0fc5d83818302ec
NouraFL <nour3_@hotmail.com>
2018-11-08 16:11:24 +0300

    done with log in devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
+ 
   resources :posts
   resources :experts
   resources :services
-  devise_for :admins
+  devise_for :users
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "pages#home"
 end
